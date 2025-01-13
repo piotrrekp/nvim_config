bcb688d0ff0cc509065b6994cd1a1edfec9f7003
Piotr Pluciński <p.plucinski@wb.com.pl>
2025-01-13 08:14:56 +0100

    snippet do gtestów sparametryzowanych
    Change-Id: Id4cacb11bbf64216b76d52260a8dd73ed19408d0

--- lua/snippets/cpp.lua
+++ lua/snippets/cpp.lua
@@ -1,4 +1,5 @@
 local ls = require("luasnip")
+local fmt = require("luasnip.extras.fmt").fmt
 local snippet = ls.snippet
 local text_node = ls.text_node
 local insert_node = ls.insert_node
@@ -27,3 +28,28 @@ ls.add_snippets("cpp", {
 		text_node({"", "}"})
 	}),
 })
+
+
+ls.add_snippets("cpp", {
+	snippet("ptest", fmt([[
+		class {} : public {}, public ::testing::TestWithParam<{}> {{
+		}};
+
+		INSTANTIATE_TEST_SUITE_P(, {}, ::testing::Values({{
+			 {}
+		    }}));
+
+		TEST_P({}, {}) {{
+		    {}
+		}}
+    ]], {
+        i(1, "TestFixtureName"),    -- Test fixture class name
+        i(2, "BaseClass"),	    -- Base class if needed
+        i(3, "ParamType"),          -- Parameter type
+        rep(1),                     -- Test fixture class name again in TEST_P
+        i(4, "param1, param2"),     -- Parameter values
+        rep(1),                     -- Test fixture class name again in INSTANTIATE_TEST_SUITE_P
+        i(5, "TestName"),           -- Test name
+        i(6, "// Test body..."),    -- Test body
+	}))
+    })
