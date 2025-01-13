8b223f8e5e67ebdad193334d7709c099d59e11ad
Piotr Pluciński <p.plucinski@wb.com.pl>
2025-01-13 08:14:15 +0100

    usuwaj białe znaki podczas zapisu pliku
    Change-Id: Ib4329aa502ec9168cbc6c7ca541c546266cc24fe

--- init.lua
+++ init.lua
@@ -662,5 +662,13 @@ cmp.setup {
   },
 }
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+  end,
+})
+
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
